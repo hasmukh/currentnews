69686f37abad268b0612d727b1bad207123d7def
Techno Cracker <hj.maniya1991@gmail.com>
2020-04-29 20:56:29 +0530

    Initial commit with project structure

---
+++ src/main/java/com/horizonx/currentnews/CurrentNewsApplication.java
@@ -0,0 +1,13 @@
+package com.horizonx.currentnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrentNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horizonx/currentnews/CurrentNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horizonx.currentnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrentNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
